5fc622905ee8c35e40b31756a4d82b8aab7f3664
Yuzhe Chen <chenyuzhe0110@outlook.com>
2023-06-09 20:25:39 +1200

    commit

--- src/pages/NewMeetups.js
+++ src/pages/NewMeetups.js
@@ -1,10 +1,27 @@
+import { useNavigate } from 'react-router-dom';
 import NewMeetupForm from '../components/meetup/NewMeetupForm';
 
 function NewMeetups() {
+  let navigate = useNavigate();
+  function addMeetupHandler(meetupData) {
+    fetch(
+      'https://react-http-test-df523-default-rtdb.asia-southeast1.firebasedatabase.app/meetups.json',
+      {
+        method: 'POST',
+        body: JSON.stringify(meetupData),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    ).then(() => {
+      navigate('/');
+    });
+  }
+
   return (
     <section>
       <h1>Add New Meetup</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section>
   );
 }
